2ff02a221bee7886578f228b30380cd44581f393
kunyoungshin <kunyoungsin@naver.com>
2025-05-20 08:58:11 +0900

    setter 제거.

--- src/main/java/nhn/academy/model/MemberCreateCommand.java
+++ src/main/java/nhn/academy/model/MemberCreateCommand.java
@@ -11,25 +11,6 @@ public class MemberCreateCommand {
     private Role role;
     private String password;
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
-
-    public void setClazz(ClassType clazz) {
-        this.clazz = clazz;
-    }
-
-    public void setRole(Role role) {
-        this.role = role;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
 
     public String getId() {
         return id;
