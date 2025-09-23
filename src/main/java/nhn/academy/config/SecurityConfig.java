af3e146a11a5f7bae9de73ee852406a2862a8fba
kunyoungshin <kunyoungsin@naver.com>
2025-09-23 22:08:44 +0900

    filter 변경.

--- src/main/java/nhn/academy/config/SecurityConfig.java
+++ src/main/java/nhn/academy/config/SecurityConfig.java
@@ -1,28 +1,32 @@
 package nhn.academy.config;
 
-import nhn.academy.auth.CustomAuthenticationFailureHandler;
-import nhn.academy.auth.CustomAuthenticationSuccessHandler;
-import nhn.academy.auth.CustomUserDetailsService;
-import nhn.academy.auth.RedisSessionFilter;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import nhn.academy.auth.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationConverter;
+import org.springframework.security.web.authentication.AuthenticationFilter;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.preauth.PreAuthenticatedAuthenticationToken;
+
+import java.util.List;
 
 @Configuration
 public class SecurityConfig {
-
-    @Autowired
-    private RedisSessionFilter redisSessionFilter;
 
     @Autowired
     private CustomAuthenticationFailureHandler customAuthenticationFailureHandler;
@@ -38,7 +42,30 @@ public class SecurityConfig {
     }
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public AuthenticationManager authenticationManager(DaoAuthenticationProvider daoProvider,
+                                                       RedisAuthenticationProvider redisProvider) {
+        // 우선순위는 필요에 따라 조정
+        return new ProviderManager(List.of(redisProvider, daoProvider));
+    }
+
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http, AuthenticationManager authenticationManager) throws Exception {
+
+        AuthenticationFilter authenticationFilter = new AuthenticationFilter(authenticationManager, new AuthenticationConverter() {
+            @Override
+            public Authentication convert(HttpServletRequest request) {
+                if (request.getCookies() == null) return null;
+                for (Cookie cookie : request.getCookies()) {
+                    if ("SESSIONID".equals(cookie.getName())) {
+                        String sessionId = cookie.getValue();
+                        return new PreAuthenticatedAuthenticationToken(sessionId, "N/A");
+                    }
+                }
+                return null; // 토큰 없으면 인증 시도 안 함
+            }
+        });
+        authenticationFilter.setSuccessHandler((req, res, auth) -> {});
         http.csrf(AbstractHttpConfigurer::disable);
         http.formLogin((formLogin) ->
                 formLogin.loginPage("/auth/login")
@@ -57,7 +84,7 @@ public class SecurityConfig {
                         .requestMatchers("/auth/login/**").permitAll()
                         .requestMatchers(HttpMethod.POST, "/members").permitAll()
                         .anyRequest().authenticated()
-        ).addFilterBefore(redisSessionFilter, UsernamePasswordAuthenticationFilter.class);
+        ).addFilterBefore(authenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
         // csrf diable
         http.csrf(AbstractHttpConfigurer::disable);
